c93ad30a542ebf8579bed15214f702617c8f4458
GeistInDerSH <geistindersh@gmail.com>
2023-12-02 11:05:36 -0700

    Update to Share Parser Logic
    Instead of acting on bare types, we can parse the data into a structure that can then be operated on more simply. This makes the logic of the operations much more clear, and the "getting" of the data more abstract

--- src/main/kotlin/day2/day2.kt
+++ src/main/kotlin/day2/day2.kt
@@ -3,50 +3,65 @@ package day2
 import helper.fileToStream
 import helper.report
 
-fun part1(fileName: String): Int {
-    val cubeCounts = mapOf("red" to 12, "blue" to 14, "green" to 13)
+data class Pull(val red: Int, val blue: Int, val green: Int)
+data class Game(val id: Int, val pulls: List<Pull>)
 
+fun parseInput(fileName: String): List<Game> {
     return fileToStream(fileName).mapIndexed { index, s ->
-        val i = index + 1
-        val isValid = s.replace("Game ${i}: ", "").split(";").all { sections ->
-            sections.trim().split(",").all {
-                val parts = it.trim().split(" ")
+        val pulls = s.substringAfter(':').split(";").map { sections ->
+            val cubes = sections.split(",").map { it.trim() }.map {
+                val parts = it.split(" ")
                 val name = parts.last()
                 val count = parts.first().toInt()
-                name in cubeCounts && cubeCounts[name]!! >= count
+                name to count
             }
+
+            Pull(
+                red = cubes.singleOrNull { it.first == "red" }?.second ?: 0,
+                blue = cubes.singleOrNull { it.first == "blue" }?.second ?: 0,
+                green = cubes.singleOrNull { it.first == "green" }?.second ?: 0,
+            )
+        }
+
+        Game(index + 1, pulls)
+    }.toList()
+}
+
+fun part1(games: List<Game>, cubeCounts: Pull): Int {
+    return games.sumOf { game ->
+        val isValid = game.pulls.all { pull ->
+            cubeCounts.red >= pull.red && cubeCounts.blue >= pull.blue && cubeCounts.green >= pull.green
         }
 
         if (isValid) {
-            i
+            game.id
         } else {
             0
         }
-    }.toList().sum()
+    }
 }
 
-fun part2(fileName: String): Int {
-    return fileToStream(fileName).mapIndexed { index, s ->
-        val i = index + 1
-        val minCubeCountByColor = listOf("red", "blue", "green").associateWith { 0 }.toMutableMap()
-        s.replace("Game ${i}: ", "").split(";").forEach { sections ->
-            sections.trim().split(",").forEach {
-                val parts = it.trim().split(" ")
-                val name = parts.last()
-                val count = parts.first().toInt()
-                if (name in minCubeCountByColor && minCubeCountByColor[name]!! < count) {
-                    minCubeCountByColor[name] = count
-                }
-            }
+fun part2(games: List<Game>): Int {
+    return games.sumOf { game ->
+        var red = 0
+        var blue = 0
+        var green = 0
+        game.pulls.forEach { pull ->
+            red = red.coerceAtLeast(pull.red)
+            blue = blue.coerceAtLeast(pull.blue)
+            green = green.coerceAtLeast(pull.green)
         }
-        minCubeCountByColor.values.fold(1) { acc: Int, idx: Int -> acc * idx }
-    }.toList().sum()
+
+        red * blue * green
+    }
 }
 
 fun day2() {
+    val input = parseInput("src/main/resources/day_2/part_1.txt")
+    val cubeCounts = Pull(red = 12, blue = 14, green = 13)
     report(
         dayNumber = 2,
-        part1 = part1("src/main/resources/day_2/part_1.txt"),
-        part2 = part2("src/main/resources/day_2/part_1.txt")
+        part1 = part1(input, cubeCounts),
+        part2 = part2(input)
     )
 }
