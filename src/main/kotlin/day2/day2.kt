933d744cb8bca92d57eb566e0ba8899be107b71f
GeistInDerSH <geistindersh@gmail.com>
2023-12-02 01:05:40 -0700

    Add Day 2 Solution
    Add solution for AoC Day 2

---
+++ src/main/kotlin/day2/day2.kt
@@ -0,0 +1,49 @@
+package day2
+
+import helper.fileToStream
+
+fun part1(fileName: String): Int {
+    val c = mapOf("red" to 12, "blue" to 14, "green" to 13)
+
+    return fileToStream(fileName).mapIndexed { index, s ->
+        val i = index + 1
+        val isValid = s.replace("Game ${i}: ", "").split(";").all { sections ->
+            sections.trim().split(",").all {
+                val parts = it.trim().split(" ")
+                val name = parts.last()
+                val count = parts.first().toInt()
+                name in c && c[name]!! >= count
+            }
+        }
+
+        if (isValid) {
+            i
+        } else {
+            0
+        }
+    }.toList().sum()
+}
+
+fun part2(fileName: String): Int {
+    return fileToStream(fileName).mapIndexed { index, s ->
+        val i = index + 1
+        val m = listOf("red", "blue", "green").associateWith { 0 }.toMutableMap()
+        s.replace("Game ${i}: ", "").split(";").forEach { sections ->
+            sections.trim().split(",").forEach {
+                val parts = it.trim().split(" ")
+                val name = parts.last()
+                val count = parts.first().toInt()
+                if (name in m && m[name]!! < count) {
+                    m[name] = count
+                }
+            }
+        }
+        m.values.fold(1) { acc: Int, idx: Int -> acc * idx }
+    }.toList().sum()
+}
+
+fun day2() {
+    println("Day 2")
+    println(part1("src/main/resources/day_2/part_1.txt"))
+    println(part2("src/main/resources/day_2/part_1.txt"))
+}
