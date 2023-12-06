409a3a210488664fba09447644255ceca8479251
GeistInDerSH <geistindersh@gmail.com>
2023-12-06 00:03:05 -0700

    Re-enable previous days, and skip day 5
    Day 5 takes too long to calculate, so skip it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
-import day5.day5
+import day1.day1
+import day2.day2
+import day3.day3
+import day4.day4
 
 fun main() {
-    // day1()
-    // day2()
-    // day3()
-    // day4()
-    day5()
+    day1()
+    day2()
+    day3()
+    day4()
+    // day5()
 }
