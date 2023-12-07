1c9baa9e199745e89236cf93b2b903eeb26f80d7
GeistInDerSH <geistindersh@gmail.com>
2023-12-07 13:20:50 -0700

    Major Performance Improvement
    Improve the speed of Day 5 by over 2 minutes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import day1.day1
 import day2.day2
 import day3.day3
 import day4.day4
+import day5.day5
 import day6.day6
 import day7.day7
 
@@ -10,7 +11,7 @@ fun main() {
     day2()
     day3()
     day4()
-    // day5()
+    day5()
     day6()
     day7()
 }
--- src/main/kotlin/day5/day5.kt
+++ src/main/kotlin/day5/day5.kt
@@ -3,34 +3,25 @@ package day5
 import helper.fileToStream
 import helper.report
 
-data class Almanac(val seeds: List<Long>, val mappings: List<Mapping>) {
-    private val destToCropMapping = mappings.associate { map -> map.destName to map.crops }
-
-    private fun getMappedValue(dest: String, value: Long) =
-        destToCropMapping[dest]?.firstNotNullOfOrNull { it.getMappedValue(value) } ?: value
-
+data class Almanac(val seeds: List<Long>, val mappings: List<List<CropMapping>>) {
     fun seedToLocation(seed: Long): Long {
-        val soil = getMappedValue("soil", seed)
-        val fertilizer = getMappedValue("fertilizer", soil)
-        val water = getMappedValue("water", fertilizer)
-        val light = getMappedValue("light", water)
-        val temperature = getMappedValue("temperature", light)
-        val humidity = getMappedValue("humidity", temperature)
-        return getMappedValue("location", humidity)
+        var value = seed
+        for (row in mappings.indices) {
+            for (col in mappings[row].indices) {
+                val crop = mappings[row][col]
+                if (crop.sourceStart <= value && value < crop.sourceEnd) {
+                    value += crop.offset
+                    break
+                }
+            }
+        }
+        return value
     }
 }
 
-data class Mapping(val destName: String, val crops: List<CropMapping>)
-
 data class CropMapping(val sourceStart: Long, val destStart: Long, val size: Long) {
-    private val sourceEnd = sourceStart + size
-    fun getMappedValue(value: Long): Long? {
-        return if (value in sourceStart..<sourceEnd) {
-            destStart + value - sourceStart
-        } else {
-            null
-        }
-    }
+    val sourceEnd = sourceStart + size
+    val offset = destStart - sourceStart
 }
 
 fun parseInput(fileName: String): Almanac {
@@ -38,16 +29,13 @@ fun parseInput(fileName: String): Almanac {
     val seeds = lines.first().substringAfter(':').trim().split(' ').map { it.toLong() }
 
     val mappings = lines.drop(1).map { line ->
-        val parts = line.split('\n')
-        val name = parts[0].substringBefore(' ').substringAfter("to-")
-        val crops = parts.drop(1).map {
+        line.split('\n').drop(1).map {
             val nums = it.split(' ').map { num -> num.toLong() }
             val dest = nums[0]
             val src = nums[1]
             val size = nums[2]
             CropMapping(src, dest, size)
         }
-        Mapping(name, crops)
     }
 
     return Almanac(seeds, mappings)
@@ -56,13 +44,10 @@ fun parseInput(fileName: String): Almanac {
 fun part1(almanac: Almanac) = almanac.seeds.map { almanac.seedToLocation(it) }.minBy { it }
 
 fun part2(almanac: Almanac): Long {
-    return almanac.seeds.chunked(2).parallelStream().map { pair ->
-        val start = pair.first()
-        val length = pair.last()
-        var min = Long.MAX_VALUE
-        (start..<start + length).forEach { min = min.coerceAtMost(almanac.seedToLocation(it)) }
-        min
-    }.toList().minBy { it }
+    return almanac.seeds.chunked(2).parallelStream()
+        .map { (start, length) ->
+            (start..<start + length).minOf { almanac.seedToLocation(it) }
+        }.toList().minOf { it }
 }
 
 fun day5() {
