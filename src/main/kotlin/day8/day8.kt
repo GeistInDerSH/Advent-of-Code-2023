99ab199a7cec3b3d093aefcee7e0542a06b25360
GeistInDerSH <geistindersh@gmail.com>
2023-12-08 10:02:34 -0700

    Code Cleanup
    The solution function can be moved to the Walk class to share it between parts, and the LCM reduction in part 2 can become a fold operation

--- src/main/kotlin/day8/day8.kt
+++ src/main/kotlin/day8/day8.kt
@@ -4,7 +4,21 @@ import helper.fileToStream
 import helper.report
 import kotlin.math.max
 
-data class Walk(val instructions: String, val nodes: Map<String, Pair<String, String>>)
+data class Walk(val instructions: String, val nodes: Map<String, Pair<String, String>>) {
+    fun getStepsCount(start: String, end: String): Long {
+        var count = 0L
+        var index = 0
+        var key = start
+        while (!key.endsWith(end)) {
+            val isLeft = instructions[index] == 'L'
+            val pair = nodes[key]!!
+            key = if (isLeft) pair.first else pair.second
+            index = (index + 1) % instructions.length
+            count += 1
+        }
+        return count
+    }
+}
 
 fun parseInput(fileName: String): Walk {
     val lines = fileToStream(fileName).toList()
@@ -17,23 +31,6 @@ fun parseInput(fileName: String): Walk {
     }
 
     return Walk(instructions, nodes)
-}
-
-fun part1(walk: Walk): Int {
-    var key = "AAA"
-    var count = 0
-    var index = 0
-    while (key != "ZZZ") {
-        val pair = walk.nodes[key]!!
-        key = if (walk.instructions[index] == 'L') {
-            pair.first
-        } else {
-            pair.second
-        }
-        count += 1
-        index = if (index + 1 == walk.instructions.length) 0 else index + 1
-    }
-    return count
 }
 
 fun lcm(a: Long, b: Long): Long {
@@ -49,24 +46,10 @@ fun lcm(a: Long, b: Long): Long {
     return maxLCM
 }
 
-fun part2(walk: Walk): Long {
-    val terminationCount = walk.nodes.keys.filter { it.endsWith('A') }.parallelStream().map {
-        var count = 0L
-        var index = 0
-        var key = it
-        while (!key.endsWith('Z')) {
-            val isLeft = walk.instructions[index] == 'L'
-            val pair = walk.nodes[key]!!
-            key = if (isLeft) pair.first else pair.second
-            index = if (index + 1 == walk.instructions.length) 0 else index + 1
-            count += 1
-        }
-        count
-    }.toList()
-    var result = terminationCount[0]
-    terminationCount.drop(1).forEach { result = lcm(result, it) }
-    return result
-}
+fun part1(walk: Walk) = walk.getStepsCount("AAA", "ZZZ")
+
+fun part2(walk: Walk) = walk.nodes.keys.filter { it.endsWith('A') }.map { walk.getStepsCount(it, "Z") }
+    .fold(1L) { acc, l -> lcm(acc, l) }
 
 fun day8() {
     val input = parseInput("src/main/resources/day_8/part_1.txt")
