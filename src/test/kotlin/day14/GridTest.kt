117552ef257c62b7edfd15f66e76fcb6aa31ca86
GeistInDerSH <geistindersh@gmail.com>
2023-12-14 14:41:06 -0700

    Add Unit Tests

---
+++ src/test/kotlin/day14/GridTest.kt
@@ -0,0 +1,22 @@
+package day14
+
+import helper.DataFile
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class GridTest {
+    private val input = parseInput(DataFile.Part1)
+    private val exampleInput = parseInput(DataFile.Example)
+
+    @Test
+    fun part1() {
+        assertEquals(exampleInput.part1(), 136)
+        assertEquals(input.part1(), 109345)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(exampleInput.part2(), 64)
+        assertEquals(input.part2(), 112452)
+    }
+}
