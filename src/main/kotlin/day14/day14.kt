4bbc7c4ae64611fa5f241f6b5c1af5c59779bfe7
GeistInDerSH <geistindersh@gmail.com>
2023-12-14 13:13:36 -0700

    Add Documentation

--- src/main/kotlin/day14/day14.kt
+++ src/main/kotlin/day14/day14.kt
@@ -18,9 +18,22 @@ data class LavaRock(var row: Int, var col: Int, val char: Char) : Cloneable {
 data class Grid(val rocks: List<LavaRock>) {
     private val colMax = rocks.maxOf { it.col }
     private val rowMax = rocks.maxOf { it.row }
+
+    // Lookup table for current positions of LavaRocks for speed
     private val rockCoordinates = rocks.map { it.row to it.col }.toMutableSet()
 
+    /**
+     * Walk through each of the [LavaRock] in [rocks] and update their position so they have been moved as far as
+     * possible in the given [direction]
+     *
+     * @param rocks The list of [LavaRock] that will be updated
+     * @param direction The direction to move the [rocks]
+     * @return The updated [LavaRock] positions
+     */
     private fun tilt(rocks: List<LavaRock>, direction: Direction): List<LavaRock> {
+        // We want to make sure that the flat list of LavaRocks is sorted by the direction we are going
+        // to be moving them. Otherwise, we may move some rocks to early and find they collide with a rock that should
+        // have been moved
         val newRocks = when (direction) {
             Direction.NORTH -> rocks.sortedBy { it.row }.reversed().toMutableList()
             Direction.SOUTH -> rocks.sortedBy { it.row }.toMutableList()
@@ -37,11 +50,16 @@ data class Grid(val rocks: List<LavaRock>) {
                 val rowUpdate = rock.row + direction.rowInc
                 val colUpdate = rock.col + direction.colInc
                 val updatedCoord = rowUpdate to colUpdate
+
+                // Stop when the new position would be outside the grid
+                // or if there is already a rock at the position
                 if (rowUpdate !in 1..rowMax || colUpdate !in 1..colMax) {
                     break
                 } else if (updatedCoord in rockCoordinates) {
                     break
                 }
+
+                // Remove the existing rock from the memory, and add the updated one
                 rockCoordinates.remove(rock.row to rock.col)
                 rockCoordinates.add(updatedCoord)
                 rock.row = rowUpdate
@@ -52,11 +70,27 @@ data class Grid(val rocks: List<LavaRock>) {
         return newRocks
     }
 
+    /**
+     * Move the loose rocks on the grid in a single direction
+     *
+     * @param direction The [Direction] to move the rocks
+     * @return A new [Grid] with the updated positions
+     */
     fun tilt(direction: Direction) = Grid(tilt(rocks.map { it.clone() }, direction))
 
+    /**
+     * Rotate the Grid a set number of times, and return the updated grid
+     *
+     * @param count The number of times to rotate the [Grid]
+     * @return A new Grid with the updated positions
+     */
     fun rotate(count: Int = 1): Grid {
+        // Internally, we can keep this as a mutable value, so we aren't calling 'tilt' and generating
+        // a ton of new object for no reason
         var newRocks = rocks.map { it.clone() }
 
+        // Having repeat when count == 1 is silly, but we can avoid extra garbage when count > 1 (e.g. when we find
+        // the remaining number of times to rotate the grid
         repeat(count) {
             for (direction in listOf(Direction.NORTH, Direction.WEST, Direction.SOUTH, Direction.EAST)) {
                 newRocks = tilt(newRocks, direction)
