b64edbaf217141a539cbe4c0d24610265b73efcc
GeistInDerSH <geistindersh@gmail.com>
2023-12-14 23:46:50 -0700

    Add HASH.equals function
    Add the equals function for comparing values (not that it's needed, IntelliJ was suggesting it)

--- src/main/kotlin/day15/day15.kt
+++ src/main/kotlin/day15/day15.kt
@@ -28,6 +28,12 @@ data class HASH(val string: String) {
 
     override fun hashCode() = hashString(string)
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return string == (other as HASH).string
+    }
+
     /**
      * @return The string label, i.e. anything before the '=' or '-'
      */
