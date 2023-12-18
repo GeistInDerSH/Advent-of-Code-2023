3725870a36ed492a9c41f03aef11d7fd12f9411f
GeistInDerSH <geistindersh@gmail.com>
2023-12-18 15:10:46 -0700

    Add Day 16 Unit Tests

---
+++ src/test/kotlin/day16/MirrorGridTest.kt
@@ -0,0 +1,22 @@
+package day16
+
+import helper.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MirrorGridTest {
+    private val input = MirrorGrid.parseInput(DataFile.Part1)
+    private val exampleInput = MirrorGrid.parseInput(DataFile.Example)
+
+    @Test
+    fun part1() {
+        assertEquals(exampleInput.part1(), 46)
+        assertEquals(input.part1(), 7927)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(exampleInput.part2(), 51)
+        assertEquals(input.part2(), 8246)
+    }
+}
