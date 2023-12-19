e51a75d8b8e95fe47ee28f7cc0a35e8394776f30
GeistInDerSH <geistindersh@gmail.com>
2023-12-19 09:48:08 -0700

    Day 19 Part 2
    Refactor the Condition to be an actual Data Class so that we can interact with the parsed components of it for part 2.
    
    Add the solution for part 2

--- src/main/kotlin/day19/day19.kt
+++ src/main/kotlin/day19/day19.kt
@@ -4,32 +4,46 @@ import helper.DataFile
 import helper.fileToString
 import helper.report
 
-typealias Condition = List<(Data) -> String>
-typealias Workflow = Map<String, Condition>
+typealias Workflow = Map<String, List<Condition>>
+
+data class Condition(val key: String, val number: Int, val dest: String, val delim: Char) {
+    /**
+     * Lazy evaluate the condition into a function that takes [Data] and returns the next location
+     * to go to.
+     *
+     * Special cases:
+     * * A: Accept; Finished
+     * * R: Reject; Finished
+     * * "": Go to the next [Condition] in the list
+     */
+    val eval: (Data) -> String by lazy {
+        when (delim) {
+            '<' -> { d: Data -> if (key in d.map && d.map[key]!! < number) dest else "" }
+            '>' -> { d: Data -> if (key in d.map && d.map[key]!! > number) dest else "" }
+            else -> { _: Data -> dest }
+        }
+    }
+}
 
 data class Data(val map: Map<String, Long>) {
     fun sum() = map.values.sum()
 }
 
-fun parseInput(fileType: DataFile): Pair<Map<String, Condition>, List<Data>> {
+fun parseInput(fileType: DataFile): Pair<Map<String, List<Condition>>, List<Data>> {
     val (code, data) = fileToString(19, fileType).split("\n\n")
 
     val workflow = code.split('\n').associate { line ->
         val stage = line.substringBefore('{')
         val conditions = line.substringAfter('{').substringBefore('}').split(',').map { cond ->
             if (!cond.contains("[<>]".toRegex())) {
-                { cond }
+                Condition("", 0, cond, ' ')
             } else {
                 val delim = if ('<' in cond) '<' else '>'
                 val key = cond.substringBefore(delim)
-                val number = cond.substringAfter(delim).substringBefore(':').toLong()
+                val number = cond.substringAfter(delim).substringBefore(':').toInt()
                 val dest = cond.substringAfter(':')
 
-                if (delim == '<') {
-                    { d: Data -> if (key in d.map && d.map[key]!! < number) dest else "" }
-                } else {
-                    { d: Data -> if (key in d.map && d.map[key]!! > number) dest else "" }
-                }
+                Condition(key, number, dest, delim)
             }
         }
         stage to conditions
@@ -47,14 +61,85 @@ fun parseInput(fileType: DataFile): Pair<Map<String, Condition>, List<Data>> {
 }
 
 fun part1(workflow: Workflow, data: List<Data>): Long {
+    val termination = setOf("A", "R")
     return data.sumOf { value ->
         var workflowName = "in"
-        while (workflowName !in setOf("A", "R")) {
-            workflowName = workflow[workflowName]!!.map { it(value) }.first { it != "" }
+        while (workflowName !in termination) {
+            workflowName = workflow[workflowName]!!.map { it.eval(value) }.first { it.isNotEmpty() }
         }
         if (workflowName == "A") value.sum()
         else 0
     }
+}
+
+/**
+ * @param r1 The first range
+ * @param r2 The second range
+ * @return The intersection of the two ranges, or an empty range if there is no overlap
+ */
+fun intersect(r1: IntRange, r2: IntRange): IntRange {
+    val intersect = r1.intersect(r2)
+    return if (intersect.isEmpty()) {
+        IntRange.EMPTY
+    } else {
+        intersect.min()..intersect.max()
+    }
+}
+
+
+fun part2(workflow: Workflow): Long {
+    fun part2(ranges: Map<String, IntRange>, ruleName: String): Long {
+        if (ruleName == "A") {
+            return ranges.values.fold(1) { prod, range -> prod * range.count() }
+        } else if (ruleName == "R") {
+            return 0
+        }
+
+        val conditions = workflow[ruleName] ?: throw Exception("Failed to find workflow for rule $ruleName")
+        val mutableRange = ranges.toMutableMap()
+        var combinations = 0L
+
+        for (cond in conditions) {
+            // If any of the ranges are empty, then we know there are no valid values
+            if (ranges.any { it.value.isEmpty() }) {
+                return 0
+            }
+
+            combinations += when (cond.delim) {
+                // '<' means we want to try the values between 0 and the number - 1 and that the updated
+                // range should be AFTER the number up to the upper bound
+                '<' -> {
+                    val rangeWithBelow = mutableRange.toMutableMap()
+                    rangeWithBelow[cond.key] = intersect(rangeWithBelow[cond.key]!!, 0..<cond.number)
+                    mutableRange[cond.key] = intersect(mutableRange[cond.key]!!, cond.number..4000)
+                    part2(rangeWithBelow, cond.dest)
+                }
+
+                // '<' means we want to try the values between the number + 1 and the upperbound and that the updated
+                // range should be BEFORE the number up to 4000
+                '>' -> {
+                    val rangeWithAbove = mutableRange.toMutableMap()
+                    rangeWithAbove[cond.key] = intersect(rangeWithAbove[cond.key]!!, cond.number + 1..4000)
+                    mutableRange[cond.key] = intersect(mutableRange[cond.key]!!, 0..cond.number)
+                    part2(rangeWithAbove, cond.dest)
+                }
+
+                // We know that we will always jump to the destination without changing the range
+                ' ' -> part2(mutableRange, cond.dest)
+                else -> throw Exception("Unknown delimiter ${cond.delim}")
+            }
+        }
+
+        return combinations
+    }
+
+    val map = mapOf(
+        "x" to 1..4000,
+        "m" to 1..4000,
+        "a" to 1..4000,
+        "s" to 1..4000,
+    )
+    return part2(map, "in")
 }
 
 fun day19() {
@@ -62,6 +147,6 @@ fun day19() {
     report(
         dayNumber = 19,
         part1 = part1(workflow, data),
-        part2 = "",
+        part2 = part2(workflow),
     )
 }
