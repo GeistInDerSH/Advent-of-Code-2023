3c995bfc808de076477a1ee4d5c64a4a2c001af4
GeistInDerSH <geistindersh@gmail.com>
2023-12-19 15:45:22 -0700

    Add Day 19 Unit Tests

---
+++ src/test/kotlin/day19/GearProcessingTest.kt
@@ -0,0 +1,22 @@
+package day19
+
+import helper.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class GearProcessingTest {
+    private val input = GearProcessing.parseInput(DataFile.Part1)
+    private val exampleInput = GearProcessing.parseInput(DataFile.Example)
+
+    @Test
+    fun part1() {
+        assertEquals(exampleInput.part1(), 19114)
+        assertEquals(input.part1(), 476889)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(exampleInput.part2(), 167409079868000)
+        assertEquals(input.part2(), 132380153677887)
+    }
+}
