808a8149ebb933ea1798ee6425b2d95e50db5254
GeistInDerSH <geistindersh@gmail.com>
2023-12-20 15:43:05 -0700

    Day 20 Unit Tests

---
+++ src/test/kotlin/day20/PropagationTest.kt
@@ -0,0 +1,23 @@
+package day20
+
+import helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PropagationTest {
+    private val input = Propagation.parseInput(DataFile.Part1)
+    private val exampleInput = Propagation.parseInput(DataFile.Example)
+    private val exampleInput2 = Propagation.parseInput(DataFile.Example2)
+
+    @Test
+    fun part1() {
+        assertEquals(exampleInput.part1(), 32000000)
+        assertEquals(exampleInput2.part1(), 11687500)
+        assertEquals(input.part1(), 886347020)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(input.part2(), 233283622908263)
+    }
+}
