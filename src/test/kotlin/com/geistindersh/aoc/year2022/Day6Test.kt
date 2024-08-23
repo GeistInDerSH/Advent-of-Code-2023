2f6c6dc6409c0aa5b10a94dfee6ace40dace9680
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-23 14:05:56 -0700

    Add 2022 Day 6

---
+++ src/test/kotlin/com/geistindersh/aoc/year2022/Day6Test.kt
@@ -0,0 +1,20 @@
+package com.geistindersh.aoc.year2022
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day6Test {
+
+    @Test
+    fun part1() {
+        assertEquals(5, Day6(DataFile.Example).part1())
+        assertEquals(6, Day6(DataFile.Example2).part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(23, Day6(DataFile.Example).part2())
+        assertEquals(23, Day6(DataFile.Example2).part2())
+    }
+}
