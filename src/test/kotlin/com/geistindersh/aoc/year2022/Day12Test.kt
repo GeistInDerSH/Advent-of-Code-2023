d9712217e6ec28e4f52d430adc11330b0c271a55
Matthew Hill <GeistInDerSH@gmail.com>
2024-08-24 12:49:28 -0700

    Add 2022 Day 12

---
+++ src/test/kotlin/com/geistindersh/aoc/year2022/Day12Test.kt
@@ -0,0 +1,18 @@
+package com.geistindersh.aoc.year2022
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day12Test {
+
+    @Test
+    fun part1() {
+        assertEquals(31, Day12(DataFile.Example).part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(29, Day12(DataFile.Example).part2())
+    }
+}
