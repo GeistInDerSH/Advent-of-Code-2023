c7c632704fa55684536a26c41201e2a7d7c8dfa5
Matthew Hill <GeistInDerSH@gmail.com>
2024-10-18 15:36:45 -0700

    Remove Answer Tests
    Remove tests that require the solution text file to be committed

--- src/test/kotlin/com/geistindersh/aoc/year2022/Day20Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2022/Day20Test.kt
@@ -9,12 +9,10 @@ class Day20Test {
     @Test
     fun part1() {
         assertEquals(3, Day20(DataFile.Example).part1())
-        assertEquals(4578, Day20(DataFile.Part1).part1())
     }
 
     @Test
     fun part2() {
         assertEquals(1623178306, Day20(DataFile.Example).part2())
-        assertEquals(2159638736133, Day20(DataFile.Part1).part2())
     }
 }
