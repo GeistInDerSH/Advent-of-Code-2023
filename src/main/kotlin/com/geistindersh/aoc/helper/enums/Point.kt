622172efafe1625c58ee2420e790ca3cfd91480e
Matthew Hill <GeistInDerSH@gmail.com>
2024-10-18 22:51:29 -0700

    Allow for adding Point and Pair
    Make it so Point + Pair returns a Point

--- src/main/kotlin/com/geistindersh/aoc/helper/enums/Point.kt
+++ src/main/kotlin/com/geistindersh/aoc/helper/enums/Point.kt
@@ -3,4 +3,5 @@ package com.geistindersh.aoc.helper.enums
 data class Point(val row: Int, val col: Int) {
     operator fun plus(other: Point) = Point(row + other.row, col + other.col)
     operator fun plus(other: Direction) = Point(row + other.rowInc, col + other.colInc)
+    operator fun plus(other: Pair<Int, Int>) = Point(row + other.first, col + other.second)
 }
