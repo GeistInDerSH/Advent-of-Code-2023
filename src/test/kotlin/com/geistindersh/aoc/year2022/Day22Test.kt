5e7043a5ffc9915f2baf5e4f6ead8d3181e46d00
Matthew Hill <GeistInDerSH@gmail.com>
2024-10-18 18:30:54 -0700

    Add Day 22 Part 1

---
+++ src/test/kotlin/com/geistindersh/aoc/year2022/Day22Test.kt
@@ -0,0 +1,17 @@
+package com.geistindersh.aoc.year2022
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+
+    @Test
+    fun part1() {
+        assertEquals(6032, Day22(DataFile.Example).part1())
+    }
+
+    @Test
+    fun part2() {
+    }
+}
