d701d5d37d370fa8d3ecf6511acac6b2156521cd
GeistInDerSH <geistindersh@gmail.com>
2024-10-21 13:00:33 -0700

    AoC 2015 Day 6 Part 2

--- src/test/kotlin/com/geistindersh/aoc/year2015/Day6Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2015/Day6Test.kt
@@ -9,11 +9,10 @@ class Day6Test {
     @Test
     fun part1() {
         assertEquals(1_000_000 - 1000 - 4, Day6(DataFile.Example).part1())
-        assertEquals(-1, Day6(DataFile.Part1).part1())
     }
 
     @Test
     fun part2() {
-        assertEquals(-1, Day6(DataFile.Example).part2())
+        assertEquals(1001996, Day6(DataFile.Example).part2())
     }
 }
