c7ed279c3a08685f64f3f88ca923fa687575e4bd
Matthew Hill <geistindersh@gmail.com>
2024-10-21 19:12:51 -0700

    AoC 2015 Day 9 Part 1

---
+++ src/test/kotlin/com/geistindersh/aoc/year2015/Day9Test.kt
@@ -0,0 +1,19 @@
+package com.geistindersh.aoc.year2015
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day9Test {
+
+    @Test
+    fun part1() {
+        assertEquals(605, Day9(DataFile.Example).part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(982, Day9(DataFile.Example).part2())
+        assertEquals(982, Day9(DataFile.Part1).part2())
+    }
+}
