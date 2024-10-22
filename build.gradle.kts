bf17ae797bfb41319aa3959189d5b2ce5e59ec70
Matthew Hill <geistindersh@gmail.com>
2024-10-22 09:32:37 -0700

    AoC 2015 Day 12 Part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,29 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    application
+	kotlin("jvm") version "1.9.21"
+	kotlin("plugin.serialization") version "2.0.20"
+	application
 }
 
 group = "com.geistindersh.adventOfCode"
 version = "2023"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+	testImplementation(kotlin("test"))
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(21)
+	jvmToolchain(21)
 }
 
 application {
-    mainClass.set("com.geistindersh.aoc.MainKt")
+	mainClass.set("com.geistindersh.aoc.MainKt")
 }
--- src/test/kotlin/com/geistindersh/aoc/year2015/Day12Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2015/Day12Test.kt
@@ -8,11 +8,11 @@ class Day12Test {
 
 	@Test
 	fun part1() {
-		assertEquals(12, Day12(DataFile.Example).part1())
+		assertEquals(6, Day12(DataFile.Example).part1())
 	}
 
 	@Test
 	fun part2() {
-		assertEquals(-1, Day12(DataFile.Example).part2())
+		assertEquals(4, Day12(DataFile.Example).part2())
 	}
 }
