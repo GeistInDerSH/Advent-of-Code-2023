6a3e5bb94ba86980da0e4eb9b61daaeec3189691
Matthew Hill <geistindersh@gmail.com>
2024-10-21 19:12:51 -0700

    AoC 2015 Day 9 Part 1

---
+++ src/main/kotlin/com/geistindersh/aoc/year2015/Day9.kt
@@ -0,0 +1,24 @@
+package com.geistindersh.aoc.year2015
+
+import com.geistindersh.aoc.helper.algorithms.toGraph
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToStream
+import com.geistindersh.aoc.helper.report
+
+class Day9(dataFile: DataFile) {
+    private val routes = fileToStream(2015, 9, dataFile)
+        .map {
+            val parts = it.split(" ")
+            Pair(parts[0], parts[2]) to parts.last().toInt()
+        }
+        .toList()
+        .toGraph()
+
+    fun part1() = routes.travelingSalesman()
+    fun part2() = 0
+}
+
+fun day9() {
+    val day = Day9(DataFile.Part1)
+    report(2015, 9, day.part1(), day.part2())
+}
