d0acec15aacaaffcffa36cd3d13fc4393fc8a42b
Matthew Hill <geistindersh@gmail.com>
2024-10-22 12:23:23 -0700

    AoC 2015 Day 13 Part 2

--- src/main/kotlin/com/geistindersh/aoc/year2015/Day13.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2015/Day13.kt
@@ -18,17 +18,28 @@ class Day13(dataFile: DataFile) {
 		.toMap()
 	private val people = data.flatMap { it.key.toList() }.toSet()
 
-	fun part1() = people
+	private fun optimalSeatingHappiness(people: Set<String>, happinessMap: Map<Pair<String, String>, Int>) = people
 		.permutations()
 		.maxOf { group ->
 			group.foldIndexed(0) { index, acc, us ->
 				val left = if (index - 1 < 0) group.last() else group[index - 1]
 				val right = if (index + 1 > group.lastIndex) group.first() else group[index + 1]
-				acc + data[us to left]!! + data[us to right]!!
+				acc + happinessMap[us to left]!! + happinessMap[us to right]!!
 			}
 		}
 
-	fun part2() = 0
+	fun part1() = optimalSeatingHappiness(people, data)
+
+	fun part2(): Int {
+		val us = "Us"
+		val data = data.toMutableMap().also {
+			people.forEach { person ->
+				it[person to us] = 0
+				it[us to person] = 0
+			}
+		}
+		return optimalSeatingHappiness(people + setOf(us), data)
+	}
 }
 
 fun day13() {
