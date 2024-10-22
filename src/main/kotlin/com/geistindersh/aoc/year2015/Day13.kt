d7d6e614f5a9a0d9dfc7d45ac61f9cae6f9daa6b
Matthew Hill <geistindersh@gmail.com>
2024-10-22 12:11:13 -0700

    AoC 2015 Day 13 Part 1

---
+++ src/main/kotlin/com/geistindersh/aoc/year2015/Day13.kt
@@ -0,0 +1,37 @@
+package com.geistindersh.aoc.year2015
+
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToStream
+import com.geistindersh.aoc.helper.iterators.permutations
+import com.geistindersh.aoc.helper.report
+
+class Day13(dataFile: DataFile) {
+	private val data = fileToStream(2015, 13, dataFile)
+		.map { line ->
+			val parts = line.split(" ")
+			val person1 = parts[0]
+			val person2 = parts[parts.lastIndex].replace(".", "")
+			val sign = if (parts[2] == "lose") -1 else 1
+			val happiness = parts[3].toInt()
+			(person1 to person2) to (sign * happiness)
+		}
+		.toMap()
+	private val people = data.flatMap { it.key.toList() }.toSet()
+
+	fun part1() = people
+		.permutations()
+		.maxOf { group ->
+			group.foldIndexed(0) { index, acc, us ->
+				val left = if (index - 1 < 0) group.last() else group[index - 1]
+				val right = if (index + 1 > group.lastIndex) group.first() else group[index + 1]
+				acc + data[us to left]!! + data[us to right]!!
+			}
+		}
+
+	fun part2() = 0
+}
+
+fun day13() {
+	val day = Day13(DataFile.Part1)
+	report(2015, 13, day.part1(), day.part2())
+}
---
+++ src/test/kotlin/com/geistindersh/aoc/year2015/Day13Test.kt
@@ -0,0 +1,18 @@
+package com.geistindersh.aoc.year2015
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day13Test {
+
+	@Test
+	fun part1() {
+		assertEquals(330, Day13(DataFile.Example).part1())
+	}
+
+	@Test
+	fun part2() {
+		assertEquals(-1, Day13(DataFile.Example).part2())
+	}
+}
