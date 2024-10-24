08115dbd6df39b6aec44609be658d9b699449bda
Matthew Hill <geistindersh@gmail.com>
2024-10-23 20:58:04 -0700

    AoC 2015 Day 25

---
+++ src/main/kotlin/com/geistindersh/aoc/year2015/Day25.kt
@@ -0,0 +1,37 @@
+package com.geistindersh.aoc.year2015
+
+import com.geistindersh.aoc.helper.enums.Point
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToString
+import com.geistindersh.aoc.helper.iterators.takeWhileInclusive
+import com.geistindersh.aoc.helper.report
+
+class Day25(dataFile: DataFile) {
+	private val targetPoint = fileToString(2015, 25, dataFile)
+		.replace(".", "")
+		.replace(",", "")
+		.split(" ")
+		.mapNotNull(String::toIntOrNull)
+		.let { Point(it[0], it[1]) }
+
+	private data class Code(val point: Point, val code: Long) {
+		fun next(): Code {
+			val (row, col) = point
+			val newPoint = if (row - 1 < 1) Point(col + 1, 1) else Point(row - 1, col + 1)
+			val newCode = (code * 252533) % 33554393
+			return Code(newPoint, newCode)
+		}
+	}
+
+	fun part1() = generateSequence(Code(Point(1, 1), 20151125)) { it.next() }
+		.takeWhileInclusive { it.point != targetPoint }
+		.last()
+		.code
+
+	fun part2() = "Push the button!"
+}
+
+fun day25() {
+	val day = Day25(DataFile.Part1)
+	report(2015, 25, day.part1(), day.part2())
+}
