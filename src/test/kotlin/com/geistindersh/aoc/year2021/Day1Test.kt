322e95873b2bcac554c46d27e7d7841822fb07c9
Matthew Hill <geistindersh@gmail.com>
2024-10-24 11:12:58 -0700

    Remove Solution Tests

--- src/test/kotlin/com/geistindersh/aoc/year2021/Day1Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2021/Day1Test.kt
@@ -9,12 +9,10 @@ class Day1Test {
 	@Test
 	fun part1() {
 		assertEquals(7, Day1(DataFile.Example).part1())
-		assertEquals(1791, Day1(DataFile.Part1).part1())
 	}
 
 	@Test
 	fun part2() {
 		assertEquals(5, Day1(DataFile.Example).part2())
-		assertEquals(1822, Day1(DataFile.Part1).part2())
 	}
 }
