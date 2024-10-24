72b0c8131527c67498d05fa7c2cc06bcd5863cda
Matthew Hill <geistindersh@gmail.com>
2024-10-23 20:59:40 -0700

    Add AoC 2015 Runner to Main

--- src/main/kotlin/com/geistindersh/aoc/Main.kt
+++ src/main/kotlin/com/geistindersh/aoc/Main.kt
@@ -1,9 +1,11 @@
 package com.geistindersh.aoc
 
+import com.geistindersh.aoc.year2015.Aoc2015
 import com.geistindersh.aoc.year2022.Aoc2022
 import com.geistindersh.aoc.year2023.Aoc2023
 
 fun main() {
-    Aoc2022.run()
-    Aoc2023.run()
+	Aoc2015.run()
+	Aoc2022.run()
+	Aoc2023.run()
 }
