d9dfebbadb7c5bfc63feed8c0f791f377f0f3330
Matthew Hill <geistindersh@gmail.com>
2024-10-24 16:03:29 -0700

    Add code to rotate all elements in an array left/right
    Allow for a list/array to have its elements shifted right or left some amount, and wrap the elements around to the other side

--- src/main/kotlin/com/geistindersh/aoc/helper/iterators/list.kt
+++ src/main/kotlin/com/geistindersh/aoc/helper/iterators/list.kt
@@ -1,5 +1,7 @@
 package com.geistindersh.aoc.helper.iterators
 
+import com.geistindersh.aoc.helper.math.positiveModulo
+
 fun <T> List<T>.pairCombinations() = sequence {
 	val seen = mutableSetOf<Pair<T, T>>()
 	for (i in this@pairCombinations) {
@@ -57,4 +59,59 @@ fun List<Int>.subsetSum(target: Int): Sequence<List<Int>> = sequence {
 				.forEach { yield(listOf(option) + it) }
 		}
 	}
+}
+
+fun <T> List<T>.rotateLeft(n: Int): List<T> {
+	val new = this.toMutableList()
+
+	this.indices.forEach { i ->
+		new[i] = this[(i + n) % this.size]
+	}
+
+	return new
+}
+
+fun IntArray.rotateLeft(n: Int): IntArray {
+	val new = this.clone()
+	this.indices.forEach { i ->
+		new[i] = this[(i + n) % this.size]
+	}
+	return new
+}
+
+fun LongArray.rotateLeft(n: Int): LongArray {
+	val new = this.clone()
+	this.indices.forEach { i ->
+		new[i] = this[(i + n) % this.size]
+	}
+	return new
+}
+
+fun <T> List<T>.rotateRight(n: Int): List<T> {
+	val new = this.toMutableList()
+
+	this.indices.forEach { i ->
+		val pos = (i - n).positiveModulo(this.size)
+		new[i] = this[pos]
+	}
+
+	return new
+}
+
+fun IntArray.rotateRight(n: Int): IntArray {
+	val new = this.clone()
+	this.indices.forEach { i ->
+		val pos = (i - n).positiveModulo(this.size)
+		new[i] = this[pos]
+	}
+	return new
+}
+
+fun LongArray.rotateRight(n: Int): LongArray {
+	val new = this.clone()
+	this.indices.forEach { i ->
+		val pos = (i - n).positiveModulo(this.size)
+		new[i] = this[pos]
+	}
+	return new
 }
