3071f174e63a80c4101693837a4d3f3f97ac41f9
Matthew Hill <geistindersh@gmail.com>
2024-10-24 12:31:58 -0700

    AoC 2021 Day 3 Parts 1 & 2

---
+++ src/test/kotlin/com/geistindersh/aoc/year2021/Day3Test.kt
@@ -0,0 +1,18 @@
+package com.geistindersh.aoc.year2021
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+
+	@Test
+	fun part1() {
+		assertEquals(198, Day3(DataFile.Example).part1())
+	}
+
+	@Test
+	fun part2() {
+		assertEquals(230, Day3(DataFile.Example).part2())
+	}
+}
