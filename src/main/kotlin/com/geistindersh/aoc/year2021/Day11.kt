7a695fd5f70fa85c84ce13e2ad6ec859cce12633
Matthew Hill <geistindersh@gmail.com>
2024-10-24 22:24:11 -0700

    AoC 2021 Day 11 Parts 1 & 2

---
+++ src/main/kotlin/com/geistindersh/aoc/year2021/Day11.kt
@@ -0,0 +1,59 @@
+package com.geistindersh.aoc.year2021
+
+import com.geistindersh.aoc.helper.enums.Point
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToStream
+import com.geistindersh.aoc.helper.report
+
+class Day11(dataFile: DataFile) {
+	private val octopuses = fileToStream(2021, 11, dataFile)
+		.flatMapIndexed { row, line ->
+			line.mapIndexed { col, power -> Point(row, col) to power.digitToInt() }
+		}
+		.toMap()
+		.let {
+			Octopuses(0, it)
+		}
+
+	private data class Octopuses(
+		val round: Int,
+		val octopuses: Map<Point, Int>,
+		val totalFlashes: Int = 0,
+		val newFlashes: Int = 0,
+	) {
+		fun next(): Octopuses {
+			val octopuses = octopuses.entries.associate { (point, value) -> point to value + 1 }.toMutableMap()
+			val flashed = mutableSetOf<Point>()
+
+			val queue = ArrayDeque<Point>()
+				.apply { addAll(octopuses.entries.filter { it.value > 9 }.map { it.key }) }
+			while (queue.isNotEmpty()) {
+				val current = queue.removeFirst()
+				if (current in flashed) continue
+				flashed.add(current)
+
+				current.neighborsAll().filter { it in octopuses }.forEach { octopuses[it] = octopuses[it]!! + 1 }
+				val newFlash = octopuses.entries.filter { it.value > 9 }.filter { it.key !in flashed }.map { it.key }
+				queue.addAll(newFlash)
+			}
+
+			flashed.forEach { octopuses[it] = 0 }
+			return Octopuses(round + 1, octopuses, totalFlashes + flashed.count(), flashed.count())
+		}
+	}
+
+	fun part1() = generateSequence(octopuses) { it.next() }
+		.drop(100)
+		.first()
+		.totalFlashes
+
+	fun part2() = generateSequence(octopuses) { it.next() }
+		.dropWhile { it.newFlashes != it.octopuses.size }
+		.first()
+		.round
+}
+
+fun day11() {
+	val day = Day11(DataFile.Part1)
+	report(2021, 11, day.part1(), day.part2())
+}
