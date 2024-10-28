c90597062899d6eafa09c3872bbdf4466c8284eb
Matthew Hill <geistindersh@gmail.com>
2024-10-27 21:37:26 -0700

    AoC 2021 Day 18 Part 2

--- src/test/kotlin/com/geistindersh/aoc/year2021/Day18Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2021/Day18Test.kt
@@ -8,12 +8,11 @@ class Day18Test {
 
 	@Test
 	fun part1() {
-		assertEquals(3488, Day18(DataFile.Example).part1())
+		assertEquals(4140, Day18(DataFile.Example).part1())
 	}
 
 	@Test
 	fun part2() {
-		assertEquals(-1, Day18(DataFile.Example).part2())
-		assertEquals(-1, Day18(DataFile.Part1).part2())
+		assertEquals(3993, Day18(DataFile.Example).part2())
 	}
 }
