c4090e10c53758237a56f631306b81c33a981e50
Matthew Hill <geistindersh@gmail.com>
2024-10-28 21:23:35 -0700

    AoC 2021 Day 22 Parts 1 & 2

--- src/main/kotlin/com/geistindersh/aoc/helper/ranges/intRange.kt
+++ src/main/kotlin/com/geistindersh/aoc/helper/ranges/intRange.kt
@@ -7,12 +7,12 @@ package com.geistindersh.aoc.helper.ranges
  * @return The intersection of the two ranges, or an empty range if there is no overlap
  */
 fun IntRange.intersect(other: IntRange): IntRange {
-    val intersect = this.intersect(other.toSet())
-    return if (intersect.isEmpty()) {
-        IntRange.EMPTY
-    } else {
-        intersect.min()..intersect.max()
-    }
+	val intersect = this.intersect(other.toSet())
+	return if (intersect.isEmpty()) {
+		IntRange.EMPTY
+	} else {
+		intersect.min()..intersect.max()
+	}
 }
 
 /**
@@ -23,7 +23,7 @@ fun IntRange.intersect(other: IntRange): IntRange {
  */
 @Suppress("unused")
 fun IntRange.hasOverlap(other: IntRange): Boolean {
-    return hasOverlap(this.first, other.last, this.last, other.first)
+	return hasOverlap(this.first, other.last, this.last, other.first)
 }
 
 /**
@@ -36,9 +36,18 @@ fun IntRange.hasOverlap(other: IntRange): Boolean {
  * @return If there is an overlap between the two ranges of numbers
  */
 fun hasOverlap(start1: Int, end1: Int, start2: Int, end2: Int): Boolean {
-    return start1 <= end1 && end2 <= start2
+	return start1 <= end1 && end2 <= start2
 }
 
 fun IntRange.isFullyContained(other: IntRange): Boolean {
-    return this.toSet().containsAll(other.toSet())
+	return this.toSet().containsAll(other.toSet())
 }
+
+/**
+ * Generate a new range of two ranges that already have an overlap in their bounds.
+ *
+ * @param other The [IntRange] to generate the intersection with
+ * @return A new range, that contains both ranges
+ */
+fun IntRange.intersection(other: IntRange): IntRange =
+	maxOf(this.first, other.first)..minOf(this.last, other.last)
---
+++ src/test/kotlin/com/geistindersh/aoc/year2021/Day22Test.kt
@@ -0,0 +1,18 @@
+package com.geistindersh.aoc.year2021
+
+import com.geistindersh.aoc.helper.files.DataFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+
+	@Test
+	fun part1() {
+		assertEquals(590784, Day22(DataFile.Example).part1())
+	}
+
+	@Test
+	fun part2() {
+		assertEquals(39769202357779, Day22(DataFile.Example).part2())
+	}
+}
