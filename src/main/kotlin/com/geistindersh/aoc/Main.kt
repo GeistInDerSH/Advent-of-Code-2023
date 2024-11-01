4e46dae9326845b64bdbd5fd622d6a3f1a857527
Matthew Hill <geistindersh@gmail.com>
2024-11-01 14:21:16 -0700

    Add 2021 Runner
    Add top level class to run all code from Advent of Code 2021

--- src/main/kotlin/com/geistindersh/aoc/Main.kt
+++ src/main/kotlin/com/geistindersh/aoc/Main.kt
@@ -1,11 +1,13 @@
 package com.geistindersh.aoc
 
 import com.geistindersh.aoc.year2015.Aoc2015
+import com.geistindersh.aoc.year2021.Aoc2021
 import com.geistindersh.aoc.year2022.Aoc2022
 import com.geistindersh.aoc.year2023.Aoc2023
 
 fun main() {
 	Aoc2015.run()
 	Aoc2022.run()
+	Aoc2021.run()
 	Aoc2023.run()
 }
