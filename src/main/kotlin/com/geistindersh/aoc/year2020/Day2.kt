0abc8245fcc70b3d75a75af86dc47df9724b664e
Matthew Hill <GeistInDerSH@gmail.com>
2024-11-04 19:31:22 -0700

    AoC 2020 Day 2 Parts 1 & 2

---
+++ src/main/kotlin/com/geistindersh/aoc/year2020/Day2.kt
@@ -0,0 +1,31 @@
+package com.geistindersh.aoc.year2020
+
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToStream
+import com.geistindersh.aoc.helper.report
+
+class Day2(dataFile: DataFile) {
+    private val policies = fileToStream(2020, 2, dataFile)
+        .map { PasswordPolicies.from(it) }
+        .toList()
+
+    private data class PasswordPolicies(val min: Int, val max: Int, val char: Char, val password: String) {
+        fun hasCharCountInRange() = password.count { it == char } in min..max
+        fun hasCharAtMinMaxPosition() = (password[min - 1] == char) xor (password[max - 1] == char)
+
+        companion object {
+            fun from(str: String): PasswordPolicies {
+                val parts = str.replace(":", "").replace("-", " ").split(" ")
+                return PasswordPolicies(parts[0].toInt(), parts[1].toInt(), parts[2][0], parts[3])
+            }
+        }
+    }
+
+    fun part1() = policies.count { it.hasCharCountInRange() }
+    fun part2() = policies.count { it.hasCharAtMinMaxPosition() }
+}
+
+fun day2() {
+    val day = Day2(DataFile.Part1)
+    report(2020, 2, day.part1(), day.part2())
+}
