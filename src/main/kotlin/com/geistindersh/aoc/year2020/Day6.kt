45ade72f1641e025edbe591592226d56aaece3a7
Matthew Hill <GeistInDerSH@gmail.com>
2024-11-05 09:23:42 -0700

    AoC 2020 Day 6 Parts 1 & 2

---
+++ src/main/kotlin/com/geistindersh/aoc/year2020/Day6.kt
@@ -0,0 +1,26 @@
+package com.geistindersh.aoc.year2020
+
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToString
+import com.geistindersh.aoc.helper.report
+
+class Day6(dataFile: DataFile) {
+    private val responses = fileToString(2020, 6, dataFile)
+        .split("\n\n")
+        .map { Responses(it.split("\n")) }
+        .toList()
+
+    private data class Responses(val people: List<String>) {
+        val yesAnswers = people.flatMap { it.toSet() }.groupingBy { it }.eachCount()
+    }
+
+    fun part1() = responses.sumOf { it.yesAnswers.size }
+    fun part2() = responses
+        .flatMap { resp -> resp.yesAnswers.entries.filter { it.value == resp.people.size } }
+        .count()
+}
+
+fun day6() {
+    val day = Day6(DataFile.Part1)
+    report(2020, 6, day.part1(), day.part2())
+}
