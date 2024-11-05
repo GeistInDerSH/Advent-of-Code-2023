1e3392b4a3bb0e44c90bde0b592b8226c7887671
Matthew Hill <GeistInDerSH@gmail.com>
2024-11-04 19:09:18 -0700

    AoC 2020 Day 1 Parts 1 & 2

---
+++ src/main/kotlin/com/geistindersh/aoc/year2020/Day1.kt
@@ -0,0 +1,24 @@
+package com.geistindersh.aoc.year2020
+
+import com.geistindersh.aoc.helper.files.DataFile
+import com.geistindersh.aoc.helper.files.fileToStream
+import com.geistindersh.aoc.helper.iterators.subsetSum
+import com.geistindersh.aoc.helper.report
+
+class Day1(dataFile: DataFile) {
+    private val numbers = fileToStream(2020, 1, dataFile).map(String::toInt).toList()
+
+    private fun solution(size: Int) = numbers
+        .subsetSum(2020)
+        .filter { it.size == size }
+        .maxOf { it.fold(1, Long::times) }
+
+    fun part1() = solution(2)
+
+    fun part2() = solution(3)
+}
+
+fun day1() {
+    val day = Day1(DataFile.Part1)
+    report(2020, 1, day.part1(), day.part2())
+}
