d5a3429baccb02ec462e39cd0ff24a4fdce72ef4
Matthew Hill <GeistInDerSH@gmail.com>
2024-11-10 11:21:06 -0700

    AoC 2020 Day 22 Part 2

--- src/test/kotlin/com/geistindersh/aoc/year2020/Day22Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2020/Day22Test.kt
@@ -13,7 +13,6 @@ class Day22Test {
 
     @Test
     fun part2() {
-        assertEquals(-1, Day22(DataFile.Example).part2())
-        assertEquals(-1, Day22(DataFile.Part1).part2())
+        assertEquals(291, Day22(DataFile.Example).part2())
     }
 }
