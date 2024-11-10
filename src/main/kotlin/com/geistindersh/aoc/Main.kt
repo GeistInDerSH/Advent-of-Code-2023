11279cba732ec0e07401babb94cf290ff70421a7
Matthew Hill <GeistInDerSH@gmail.com>
2024-11-10 16:28:42 -0700

    Add 2020 Driver Code

--- src/main/kotlin/com/geistindersh/aoc/Main.kt
+++ src/main/kotlin/com/geistindersh/aoc/Main.kt
@@ -1,13 +1,15 @@
 package com.geistindersh.aoc
 
 import com.geistindersh.aoc.year2015.Aoc2015
+import com.geistindersh.aoc.year2020.Aoc2020
 import com.geistindersh.aoc.year2021.Aoc2021
 import com.geistindersh.aoc.year2022.Aoc2022
 import com.geistindersh.aoc.year2023.Aoc2023
 
 fun main() {
-	Aoc2015.run()
-	Aoc2022.run()
-	Aoc2021.run()
-	Aoc2023.run()
+    Aoc2015.run()
+    Aoc2020.run()
+    Aoc2022.run()
+    Aoc2021.run()
+    Aoc2023.run()
 }
