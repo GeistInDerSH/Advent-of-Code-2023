f392d64e4cbce91681c51587868000e720b0d49b
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-01 22:48:34 -0700

    AoC 2024 Day 2 Part 2

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day2.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day2.kt
@@ -13,32 +13,31 @@ class Day2(
             .map { line -> "[0-9]+".toRegex().findAll(line).map { it.value.toInt() }.toList() }
             .toList()
 
-    fun part1(): Int {
-        var count = 0
-
-        for (seq in data) {
-            val isIncreasing = seq[0] < seq[1]
-            var all = true
-            for ((a, b) in seq.windowed(2)) {
-                if (isIncreasing && a > b) {
-                    all = false
-                    break
-                } else if (!isIncreasing && a < b) {
-                    all = false
-                    break
-                } else if ((a - b).absoluteValue !in 1..3) {
-                    all = false
-                    break
-                }
+    private fun List<Int>.isSafe(): Boolean {
+        val isIncreasing = this[0] < this[1]
+        for ((a, b) in this.windowed(2)) {
+            when {
+                isIncreasing && a > b -> return false
+                !isIncreasing && a < b -> return false
+                (a - b).absoluteValue !in 1..3 -> return false
             }
-
-            if (all) count++
         }
-
-        return count
+        return true
     }
 
-    fun part2() = 0
+    fun part1() = data.count { it.isSafe() }
+
+    fun part2() =
+        data
+            .map { seq ->
+                if (seq.isSafe()) {
+                    1
+                } else {
+                    val anyValid =
+                        seq.indices.any { seq.toMutableList().apply { removeAt(it) }.isSafe() }
+                    if (anyValid) 1 else 0
+                }
+            }.sum()
 }
 
 fun day2() {
