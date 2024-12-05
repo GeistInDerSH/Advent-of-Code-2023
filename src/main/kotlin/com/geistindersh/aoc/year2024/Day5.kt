445bd8f18d33af606e2a6c6479bdd8378a491603
Matthew Hill <geistindersh@gmail.com>
2024-12-05 08:53:10 -0700

    Performance Optimizations
    Avoid using what effectively amounts to Pair<Int, Int> to store the rules, and instead store them in a Map<Int, Set<Int>> where the left-hand-side is the key, and the right-hand-side(s) are the values in the set. This allows for faster and simpler order checking and sorting, because we only need to do 2 hash checks in the worst case.

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day5.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day5.kt
@@ -10,35 +10,39 @@ class Day5(
     private val data =
         run {
             val parts = fileToString(2024, 5, dataFile).split("\n\n")
-            val pages =
-                parts[0]
-                    .split("\n")
-                    .map { line -> line.split("|").map { it.toInt() }.let { Pages(it[0], it[1]) } }
-                    .toSet()
+            val pages: Map<Int, Set<Int>> =
+                run {
+                    val map = mutableMapOf<Int, MutableSet<Int>>()
+                    for (line in parts[0].split("\n")) {
+                        val k = line.substringBefore("|").toInt()
+                        val v = line.substringAfter("|").toInt()
+                        if (k in map) {
+                            map[k]!!.add(v)
+                        } else {
+                            map[k] = mutableSetOf(v)
+                        }
+                    }
+                    map
+                }
             val updates =
                 parts[1]
                     .split("\n")
                     .map { line -> line.split(",").map { it.toInt() } }
+                    .toSet()
             pages to updates
         }
     private val pages = data.first
     private val updates = data.second
 
-    private data class Pages(
-        val left: Int,
-        val right: Int,
-    )
-
     private fun List<Int>.hasCorrectOrder() =
-        this
-            .windowed(2)
-            .all { (a, b) -> Pages(a, b) in pages }
+        (0..<this.lastIndex)
+            .all { i ->
+                val a = this[i]
+                val b = this[i + 1]
+                a in pages && b in pages[a]!!
+            }
 
-    private fun List<Int>.fixOrder() =
-        this.sortedWith { a, b ->
-            val options = pages.filter { it.right == a }.map { it.left }.toSet()
-            if (b in options) -1 else 0
-        }
+    private fun List<Int>.fixOrder() = this.sortedWith { a, b -> if (a in pages && b in pages[a]!!) -1 else 0 }
 
     fun part1() =
         updates
