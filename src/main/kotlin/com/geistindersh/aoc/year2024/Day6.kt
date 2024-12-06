d8534b3c3ce804df609a555f4b7e81ae4987b670
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-06 01:33:55 -0700

    AoC 2024 Day 6 Code Cleanup

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day6.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day6.kt
@@ -9,12 +9,12 @@ import com.geistindersh.aoc.helper.report
 class Day6(
     dataFile: DataFile,
 ) {
-    private val data =
+    private val grid =
         fileToStream(2024, 6, dataFile)
             .flatMapIndexed { row, line ->
                 line.mapIndexed { col, value -> Point2D(row, col) to value }
             }.toMap()
-    private val start = data.filterValues { it == '^' }.keys.first()
+    private val start = grid.filterValues { it == '^' }.keys.first()
 
     private fun Map<Point2D, Char>.hasLoop(): Boolean {
         val memory = mutableSetOf<Pair<Point2D, Direction>>()
@@ -35,27 +35,23 @@ class Day6(
     private fun traverse() =
         generateSequence(start to Direction.North) { (pos, dir) ->
             val newPos = pos + dir
-            if (data.getOrDefault(newPos, '.') == '#') {
+            if (grid.getOrDefault(newPos, '.') == '#') {
                 pos to dir.turnRight()
             } else {
                 newPos to dir
             }
         }.map { it.first }
-            .takeWhile { it in data }
+            .takeWhile { it in grid }
             .toSet()
 
     fun part1() = traverse().count()
 
-    fun part2(): Int {
-        val path = traverse().toMutableSet().apply { remove(start) }
-
-        return path.count { point ->
-            data
-                .toMutableMap()
-                .also { it[point] = '#' }
-                .hasLoop()
-        }
-    }
+    fun part2(): Int =
+        traverse()
+            .parallelStream()
+            .map { point -> if (grid.toMutableMap().also { it[point] = '#' }.hasLoop()) 1 else 0 }
+            .reduce(Int::plus)
+            .orElse(-1)
 }
 
 fun day6() {
