80924df74c8d0be927cdfeb0f22e08964d333b90
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-08 23:38:48 -0700

    AoC 2024 Day 9 Part 2

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day9.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day9.kt
@@ -64,16 +64,76 @@ class Day9(
         return this
     }
 
-    fun part1() =
-        data.toArray().fillEmpty().foldIndexed(0L) { idx, acc, v ->
-            if (v == -1) {
-                acc
-            } else {
-                acc + (idx * v)
+    private fun IntArray.emptyBlockRanges(): MutableList<IntRange> {
+        val emptyBlockRanges = mutableListOf<IntRange>()
+        var i = 0
+        while (i < this.size) {
+            if (this[i] != -1) {
+                i += 1
+                continue
+            }
+            var end = i
+            while (this[end] == -1) end += 1
+            emptyBlockRanges.add(i..<end)
+            i = end
+        }
+        return emptyBlockRanges
+    }
+
+    private fun List<DiskMap>.fillArrayContiguous(): IntArray {
+        val arr = this.toArray()
+
+        // Get ranges for the known empty blocks that we can later fill
+        val emptyBlockRanges = arr.emptyBlockRanges()
+
+        for (file in this.reversed()) {
+            when (file) {
+                is DiskMap.EmptyBlock -> continue
+                is DiskMap.BlockFile -> {
+                    val insertBlock = emptyBlockRanges.firstOrNull { it.count() >= file.size } ?: continue
+                    val fileStart = arr.indexOfFirst { it == file.id }
+                    if (insertBlock.first > fileStart) continue
+                    for (i in 0..<file.size) {
+                        arr[i + insertBlock.first] = file.id
+                        arr[i + fileStart] = -1
+                    }
+
+                    emptyBlockRanges.remove(insertBlock)
+                    if (file.size != insertBlock.count()) {
+                        emptyBlockRanges.add(insertBlock.first + file.size..insertBlock.last)
+                    }
+
+                    emptyBlockRanges.add(fileStart..fileStart + file.size)
+                    emptyBlockRanges.sortBy { it.first }
+                }
             }
         }
 
-    fun part2() = 0
+        return arr
+    }
+
+    fun part1() =
+        data
+            .toArray()
+            .fillEmpty()
+            .foldIndexed(0L) { idx, acc, v ->
+                if (v == -1) {
+                    acc
+                } else {
+                    acc + (idx * v)
+                }
+            }
+
+    fun part2() =
+        data
+            .fillArrayContiguous()
+            .foldIndexed(0L) { idx, acc, v ->
+                if (v == -1) {
+                    acc
+                } else {
+                    acc + (idx * v)
+                }
+            }
 }
 
 fun day9() {
