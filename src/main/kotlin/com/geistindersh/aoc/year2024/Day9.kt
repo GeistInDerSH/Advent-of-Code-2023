6cc6b21183eb80d204397204f17722bbf32cae44
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-08 23:56:36 -0700

    AoC 2024 Day 9 Cleanup & Documentation
    Rename variables to better indicate what they are for, and functions to clarify what they do.
    Add documentation in-line to clarify what is being done, and why. Add docstrings to functions

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day9.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day9.kt
@@ -7,7 +7,7 @@ import com.geistindersh.aoc.helper.report
 class Day9(
     dataFile: DataFile,
 ) {
-    private val data =
+    private val blockSectors =
         fileToString(2024, 9, dataFile)
             .map(Char::digitToInt)
             .windowed(2, 2, true)
@@ -32,30 +32,44 @@ class Day9(
         ) : DiskMap(size)
     }
 
+    /**
+     * Convert the list of [DiskMap] into an [IntArray].
+     *
+     * The id of each of the files is inserted at an appropriate index to match its block
+     * offset. Empty locations are denoted with a value of '-1'.
+     *
+     * @return The block location of each of the files
+     */
     private fun List<DiskMap>.toArray(): IntArray {
         val size = this.sumOf { it.size }
-        val arr = IntArray(size) { -1 }
+        val fileArray = IntArray(size) { -1 }
         var i = 0
         for (file in this) {
             when (file) {
+                is DiskMap.EmptyBlock -> {}
                 is DiskMap.BlockFile -> {
                     for (j in 0..<file.size) {
-                        arr[i + j] = file.id
+                        fileArray[i + j] = file.id
                     }
-                    i += file.size
-                }
-                is DiskMap.EmptyBlock -> {
-                    i += file.size
                 }
             }
+            i += file.size
         }
-        return arr
+        return fileArray
     }
 
-    private fun IntArray.fillEmpty(): IntArray {
+    /**
+     * Compact the array by moving the right most value(s) into the left most open spot(s).
+     *
+     * Note that the [IntArray] is modified in place, and the value is returned to allow for chaining.
+     *
+     * @return The modified IntArray
+     */
+    private fun IntArray.compactFragmenting(): IntArray {
         var firstEmptyIndex = this.indexOfFirst { it == -1 }
         var lastFilledIndex = this.indexOfLast { it != -1 }
         while (lastFilledIndex > firstEmptyIndex && firstEmptyIndex != -1) {
+            // Move the single block
             this[firstEmptyIndex] = this[lastFilledIndex]
             this[lastFilledIndex] = -1
             firstEmptyIndex = this.indexOfFirst { it == -1 }
@@ -64,6 +78,11 @@ class Day9(
         return this
     }
 
+    /**
+     * Get the start and end indices for each "blank" block of data
+     *
+     * @return A mutable list of the ranges
+     */
     private fun IntArray.emptyBlockRanges(): MutableList<IntRange> {
         val emptyBlockRanges = mutableListOf<IntRange>()
         var i = 0
@@ -80,60 +99,59 @@ class Day9(
         return emptyBlockRanges
     }
 
-    private fun List<DiskMap>.fillArrayContiguous(): IntArray {
-        val arr = this.toArray()
+    /**
+     * Attempt to compact file locations by moving whole files into the right-most space
+     * with enough contiguous memory.
+     *
+     * @return The compacted memory
+     */
+    private fun List<DiskMap>.toCompactArrayContiguous(): IntArray {
+        val fileArray = this.toArray()
 
         // Get ranges for the known empty blocks that we can later fill
-        val emptyBlockRanges = arr.emptyBlockRanges()
+        val emptyBlockRanges = fileArray.emptyBlockRanges()
 
         for (file in this.reversed()) {
             when (file) {
                 is DiskMap.EmptyBlock -> continue
                 is DiskMap.BlockFile -> {
                     val insertBlock = emptyBlockRanges.firstOrNull { it.count() >= file.size } ?: continue
-                    val fileStart = arr.indexOfFirst { it == file.id }
+                    val fileStart = fileArray.indexOfFirst { it == file.id }
+                    // Check that the first open spot is before the current file location
                     if (insertBlock.first > fileStart) continue
+
+                    // Move the file, and clear the old position
                     for (i in 0..<file.size) {
-                        arr[i + insertBlock.first] = file.id
-                        arr[i + fileStart] = -1
+                        fileArray[i + insertBlock.first] = file.id
+                        fileArray[i + fileStart] = -1
                     }
 
+                    // Remove, and possibly adjust the free space we just filled with the moved file
                     emptyBlockRanges.remove(insertBlock)
                     if (file.size != insertBlock.count()) {
                         emptyBlockRanges.add(insertBlock.first + file.size..insertBlock.last)
                     }
 
+                    // Add the cleared file range, and reorder the blocks by the first index free
                     emptyBlockRanges.add(fileStart..fileStart + file.size)
                     emptyBlockRanges.sortBy { it.first }
                 }
             }
         }
 
-        return arr
+        return fileArray
     }
 
     fun part1() =
-        data
+        blockSectors
             .toArray()
-            .fillEmpty()
-            .foldIndexed(0L) { idx, acc, v ->
-                if (v == -1) {
-                    acc
-                } else {
-                    acc + (idx * v)
-                }
-            }
+            .compactFragmenting()
+            .foldIndexed(0L) { idx, acc, v -> if (v == -1) acc else acc + (idx * v) }
 
     fun part2() =
-        data
-            .fillArrayContiguous()
-            .foldIndexed(0L) { idx, acc, v ->
-                if (v == -1) {
-                    acc
-                } else {
-                    acc + (idx * v)
-                }
-            }
+        blockSectors
+            .toCompactArrayContiguous()
+            .foldIndexed(0L) { idx, acc, v -> if (v == -1) acc else acc + (idx * v) }
 }
 
 fun day9() {
