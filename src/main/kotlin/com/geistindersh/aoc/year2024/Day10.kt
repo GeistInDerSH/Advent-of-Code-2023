43d831797cea712442ca2acf01ccf4910efbe2f0
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-10 08:42:56 -0700

    Share Code Between Parts 1 & 2
    There is really only a small difference between the two parts, and code can easily be shared between them to reduce complexity and run time

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day10.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day10.kt
@@ -4,69 +4,49 @@ import com.geistindersh.aoc.helper.enums.Point2D
 import com.geistindersh.aoc.helper.files.DataFile
 import com.geistindersh.aoc.helper.files.fileToStream
 import com.geistindersh.aoc.helper.report
-import java.util.PriorityQueue
 
 class Day10(
     dataFile: DataFile,
 ) {
-    private val data =
+    private val trailMap =
         fileToStream(2024, 10, dataFile)
             .flatMapIndexed { row, line ->
                 line.mapIndexed { col, digit -> Point2D(row, col) to digit.digitToInt() }
             }.toMap()
-    private val startingPoints = data.filterValues { it == 0 }.keys
+    private val paths = trailMap.filterValues { it == 0 }.keys.map { it.pathsToEnd() }
 
-    private fun Point2D.trailScore(): Int {
-        val start = this to data[this]!!
-        val queue = PriorityQueue<Pair<Point2D, Int>>(compareBy { it.second }).apply { add(start) }
-        val seen = mutableSetOf<Point2D>()
-
-        while (queue.isNotEmpty()) {
-            val (point, grade) = queue.poll()
-            if (!seen.add(point)) continue
-            val nextPairs =
-                point
-                    .neighbors()
-                    .filter { it !in seen && it in data && data[it]!! == grade + 1 }
-                    .map { it to data[it]!! }
-            queue.addAll(nextPairs)
-        }
-
-        return seen.count { data[it]!! == 9 }
-    }
-
-    private fun Point2D.trailCount(): Int {
+    private fun Point2D.pathsToEnd(): Set<Set<Point2D>> {
         val start = this to emptySet<Point2D>()
-        val queue = ArrayDeque<Pair<Point2D, Set<Point2D>>>().apply { add(start) }
+        val queue = ArrayDeque(listOf(start))
         val seen = mutableSetOf<Pair<Point2D, Set<Point2D>>>()
+        val paths = mutableSetOf<Set<Point2D>>()
 
-        var count = 0
         while (queue.isNotEmpty()) {
             val head = queue.removeFirst()
             if (!seen.add(head)) continue
 
-            val (point, path) = head
-            val newPath = path.toMutableSet().apply { add(point) }
-            if (data[point]!! == 9) {
-                seen.add(point to newPath)
-                count += 1
+            val (currentPoint, traveledPath) = head
+            val path = traveledPath + currentPoint
+            if (trailMap[currentPoint]!! == 9) {
+                paths.add(path)
                 continue
             }
 
-            val nextStepUp = data[point]!! + 1
-            for (nextPoint in point.neighbors().filter { it in data && data[it] == nextStepUp }) {
-                val toAdd = nextPoint to newPath
+            val nextStepUp = trailMap[currentPoint]!! + 1
+            for (point in currentPoint.neighbors()) {
+                if (!(point in trailMap && trailMap[point] == nextStepUp)) continue
+                val toAdd = point to path
                 if (toAdd in seen) continue
                 queue.add(toAdd)
             }
         }
 
-        return count
+        return paths
     }
 
-    fun part1() = startingPoints.sumOf { it.trailScore() }
+    fun part1() = paths.sumOf { p -> p.flatMapTo(mutableSetOf()) { it }.count { trailMap[it]!! == 9 } }
 
-    fun part2() = startingPoints.sumOf { it.trailCount() }
+    fun part2() = paths.sumOf { it.size }
 }
 
 fun day10() {
