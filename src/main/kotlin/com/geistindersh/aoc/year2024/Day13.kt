96f439b40ffb5e632bbd3caa70aed136cf3b62a2
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-12 23:59:02 -0700

    AoC 2024 Day 13 Code Cleanup

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day13.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day13.kt
@@ -7,42 +7,20 @@ import com.geistindersh.aoc.helper.report
 class Day13(
     dataFile: DataFile,
 ) {
-    private val data =
+    private val games =
         fileToString(2024, 13, dataFile)
             .split("\n\n")
             .map { ClawMachine.from(it) }
             .toList()
 
-    private data class Button(
-        val x: Long,
-        val y: Long,
-    )
-
-    private data class Prize(
-        val x: Long,
-        val y: Long,
-    )
-
     private data class ClawMachine(
-        val a: Button,
-        val b: Button,
-        val prize: Prize,
+        val ax: Long,
+        val ay: Long,
+        val bx: Long,
+        val by: Long,
+        val px: Long,
+        val py: Long,
     ) {
-        fun tryGetPrize(): Pair<Int, Int>? {
-            for (aPresses in 0..<100) {
-                val ax = a.x * aPresses
-                val ay = a.y * aPresses
-                for (bPresses in 0..<100) {
-                    val bx = b.x * bPresses
-                    val by = b.y * bPresses
-                    if (ax + bx == prize.x && ay + by == prize.y) {
-                        return aPresses to bPresses
-                    }
-                }
-            }
-            return null
-        }
-
         private fun determinate(
             a: Long,
             b: Long,
@@ -50,54 +28,41 @@ class Day13(
             d: Long,
         ) = (a * d) - (b * c)
 
-        fun tryGetPrizeNoLimit(): Pair<Long, Long>? {
-            val div = determinate(a.x, a.y, b.x, b.y)
-            val x = determinate(prize.x, b.x, prize.y, b.y)
-            val y = determinate(a.x, prize.x, a.y, prize.y)
-            if (x % div != 0L || y % div != 0L) return null
+        fun tryGetPrize(): Pair<Long, Long> {
+            val (x, y) = tryGetPrizeNoLimit()
+            if (x > 100 || y > 100) return 0L to 0L
+            return x to y
+        }
+
+        fun tryGetPrizeNoLimit(): Pair<Long, Long> {
+            val div = determinate(ax, ay, bx, by)
+            val x = determinate(px, bx, py, by)
+            val y = determinate(ax, px, ay, py)
+            if (x % div != 0L || y % div != 0L) return 0L to 0L
             return (x / div) to (y / div)
         }
 
         companion object {
             private val NUMBER_REGEX = "[0-9]+".toRegex()
 
-            fun from(str: String): ClawMachine {
-                val lines = str.split("\n")
-                val a =
-                    NUMBER_REGEX
-                        .findAll(lines[0])
-                        .map { it.value.toLong() }
-                        .toList()
-                        .let { Button(it[0], it[1]) }
-                val b =
-                    NUMBER_REGEX
-                        .findAll(lines[1])
-                        .map { it.value.toLong() }
-                        .toList()
-                        .let { Button(it[0], it[1]) }
-                val prize =
-                    NUMBER_REGEX
-                        .findAll(lines[2])
-                        .map { it.value.toLong() }
-                        .toList()
-                        .let { Prize(it[0], it[1]) }
-                return ClawMachine(a, b, prize)
-            }
+            fun from(str: String): ClawMachine =
+                NUMBER_REGEX
+                    .findAll(str)
+                    .map {
+                        it.value.toLong()
+                    }.toList()
+                    .let { ClawMachine(it[0], it[1], it[2], it[3], it[4], it[5]) }
         }
     }
 
-    fun part1() =
-        data
-            .mapNotNull { it.tryGetPrize() }
-            .map { (a, b) -> a * 3 + b }
-            .sumOf { it }
+    private fun Pair<Long, Long>.score() = this.first * 3 + this.second
+
+    fun part1() = games.sumOf { it.tryGetPrize().score() }
 
     fun part2() =
-        data
-            .map { it.copy(prize = Prize(it.prize.x + 10000000000000L, it.prize.y + 10000000000000L)) }
-            .mapNotNull { it.tryGetPrizeNoLimit() }
-            .map { (a, b) -> a * 3 + b }
-            .sumOf { it }
+        games
+            .map { it.copy(px = it.px + 10000000000000L, py = it.py + 10000000000000L) }
+            .sumOf { it.tryGetPrizeNoLimit().score() }
 }
 
 fun day13() {
