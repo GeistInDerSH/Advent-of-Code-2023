7c305ee200e5c544850b645cbe51e41e9c8146c3
Matthew Hill <GeistInDerSH@gmail.com>
2024-12-15 23:58:15 -0700

    AoC 2024 Day 16 Part 2

--- src/test/kotlin/com/geistindersh/aoc/year2024/Day16Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2024/Day16Test.kt
@@ -8,12 +8,10 @@ class Day16Test {
     @Test
     fun part1() {
         assertEquals(7036, Day16(DataFile.Example).part1())
-        assertEquals(-1, Day16(DataFile.Part1).part1())
     }
 
     @Test
     fun part2() {
-        assertEquals(-1, Day16(DataFile.Example).part2())
-        assertEquals(-1, Day16(DataFile.Part1).part2())
+        assertEquals(45, Day16(DataFile.Example).part2())
     }
 }
