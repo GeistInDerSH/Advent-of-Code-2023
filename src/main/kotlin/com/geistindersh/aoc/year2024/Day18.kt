1028abc34d1b3b5cefab17359cc476a9b3fe9328
Matthew Hill <geistindersh@gmail.com>
2024-12-18 10:12:43 -0800

    AoC 2024 Day 18 Part 2

--- src/main/kotlin/com/geistindersh/aoc/year2024/Day18.kt
+++ src/main/kotlin/com/geistindersh/aoc/year2024/Day18.kt
@@ -1,21 +1,50 @@
 package com.geistindersh.aoc.year2024
 
+import com.geistindersh.aoc.helper.enums.Point2D
 import com.geistindersh.aoc.helper.files.DataFile
 import com.geistindersh.aoc.helper.files.fileToStream
 import com.geistindersh.aoc.helper.report
+import java.util.PriorityQueue
 
 class Day18(
     dataFile: DataFile,
+    private val gridMaxRow: Int = 70,
+    private val gridMaxCol: Int = 70,
 ) {
-    private val data =
+    private val bytePoints =
         fileToStream(2024, 18, dataFile)
-            .map { it.split(",").map(String::toLong) }
-            .map { (a, b) -> a to b }
+            .map { it.split(",").map(String::toInt) }
+            .map { (a, b) -> Point2D(a, b) }
             .toList()
 
-    fun part1() = 0
+    private fun Point2D.inGrid() = this.row in (0..gridMaxRow) && this.col in (0..gridMaxCol)
 
-    fun part2() = 0
+    private fun Point2D.isEnd() = this.row == gridMaxRow && this.col == gridMaxCol
+
+    private fun Set<Point2D>.traverse(): Int {
+        val queue = PriorityQueue<Pair<Point2D, Int>>(compareBy { it.second }).apply { add(Point2D(0, 0) to 0) }
+        val seen = mutableSetOf<Point2D>()
+        while (queue.isNotEmpty()) {
+            val (point, score) = queue.poll()
+            if (!seen.add(point)) continue
+            if (point.isEnd()) return score
+
+            for (next in point.neighbors()) {
+                if (!next.inGrid()) continue
+                if (next in this) continue
+                queue.add(next to score + 1)
+            }
+        }
+        return -1
+    }
+
+    fun part1(byteCount: Int = 1024) = bytePoints.take(byteCount).toSet().traverse()
+
+    fun part2() =
+        generateSequence(bytePoints.count() to bytePoints) { (it.first - 1) to bytePoints.take(it.first) }
+            .first { it.second.toSet().traverse() != -1 }
+            .first
+            .let { bytePoints[it + 1] }
 }
 
 fun day18() {
--- src/test/kotlin/com/geistindersh/aoc/year2024/Day18Test.kt
+++ src/test/kotlin/com/geistindersh/aoc/year2024/Day18Test.kt
@@ -1,5 +1,6 @@
 package com.geistindersh.aoc.year2024
 
+import com.geistindersh.aoc.helper.enums.Point2D
 import com.geistindersh.aoc.helper.files.DataFile
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -7,13 +8,11 @@ import kotlin.test.assertEquals
 class Day18Test {
     @Test
     fun part1() {
-        assertEquals(22, Day18(DataFile.Example).part1())
-        assertEquals(-1, Day18(DataFile.Part1).part1())
+        assertEquals(22, Day18(DataFile.Example, 6, 6).part1(12))
     }
 
     @Test
     fun part2() {
-        assertEquals(-1, Day18(DataFile.Example).part2())
-        assertEquals(-1, Day18(DataFile.Part1).part2())
+        assertEquals(Point2D(6, 1), Day18(DataFile.Example, 6, 6).part2())
     }
 }
